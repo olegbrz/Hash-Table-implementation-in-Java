609c56dc66b70d49a74f36e49fb95c7662a3923e
olegbrz <43888615+olegbrz@users.noreply.github.com>
2019-10-25 09:25:32 +0200

    Node class redesigned

--- src/hashTable/Node.java
+++ src/hashTable/Node.java
@@ -6,13 +6,12 @@ public class Node<K, V> {
 	
 	private K key;
 	private V value;
-	private Node<K, V> next;
+
 	
 	/* Constructor */
-	public Node(K key, V value, Node<K, V> next) {
+	public Node(K key, V value) {
 		this.key = key;
 		this.value = value;
-		this.next = next;
 	}
 
 	/* Getters and setters */
@@ -31,14 +30,5 @@ public class Node<K, V> {
 	public void setValue(V value) {
 		this.value = value;
 	}
-
-	public Node<K, V> getNext() {
-		return next;
-	}
-
-	public void setNext(Node<K, V> next) {
-		this.next = next;
-	}
-	
 	
 }
